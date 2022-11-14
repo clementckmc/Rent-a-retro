bff29fe8e12415d7b01db6ea2116cc4148f64f68
Alex V <alexandervasse@gmail.com>
2022-11-14 15:49:06 +0900

    Game migration and model created (no controller/routes needed)

---
+++ app/models/game.rb
@@ -0,0 +1,9 @@
+class Game < ApplicationRecord
+  has_many :offers
+  has_many :rentals, through :offers
+  has_many :users, through :offers
+  has_one_attached :photo
+
+  validates :name, presence: :true
+  validates :description, presence: :true
+end
