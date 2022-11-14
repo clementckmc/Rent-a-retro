2f925c299468d2ee463e7a3cc3854cd3891ee924
clementckmc <46878585+clementckmc@users.noreply.github.com>
2022-11-14 17:21:37 +0900

    User model (#4)
    * generate user model
    
    * add display_name

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :display_name, presence: true, uniquesness: true
+
+  has_many :offers
+  has_many :rentals
+end
