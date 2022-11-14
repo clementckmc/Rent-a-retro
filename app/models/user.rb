01c7943ea6cc8efb297485504bca07785e9dba24
Clement Choi <clementchoi1997@gmail.com>
2022-11-14 17:24:39 +0900

    typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :display_name, presence: true, uniquesness: true
+  validates :display_name, presence: true, uniqueness: true
 
   has_many :offers
   has_many :rentals
