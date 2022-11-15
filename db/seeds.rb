555e4232d7fe22f4ca285f3bcdc30644e2f9e1d9
Clement Choi <clementchoi1997@gmail.com>
2022-11-15 14:13:44 +0900

    faker gen 10 more users, remove private key

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,33 @@
 require 'net/https'
 require 'open-uri'
+require 'faker'
 
 User.destroy_all
 Game.destroy_all
 
-User.create(email: "fuhajin@gmail.com", first_name: "juri", last_name: "han", display_name: "jurihan")
-User.create(email: "littlefriend@gmail.com", first_name: "tony", last_name: "montana", display_name: "scarface")
-User.create(email: "quietlife@gmail.com", first_name: "yoshikage", last_name: "kira", display_name: "killerqueen")
-User.create(email: "baddad@gmail.com", first_name: "gendo", last_name: "ikari", display_name: "gendopose")
-User.create(email: "beauty@gmail.com", first_name: "audrey", last_name: "hepburn", display_name: "romanholiday")
-User.create(email: "pokerface@gmail.com", first_name: "stefani", last_name: "germanotta", display_name: "ladygaga")
-User.create(email: "illustrious@gmail.com", first_name: "mari", last_name: "makinami", display_name: "zabeasto")
-User.create(email: "bondvillian@gmail.com", first_name: "elon", last_name: "musk", display_name: "chieftwit")
-User.create(email: "darkknight@gmail.com", first_name: "bruce", last_name: "wayne", display_name: "batman")
-User.create(email: "bigguy4u@gmail.com", first_name: "tom", last_name: "hardy", display_name: "bane")
-User.create(email: "cloudff7r@gmail.com", first_name: "cloud", last_name: "strife", display_name: "cloud")
-User.create(email: "aerithff7r@gmail.com", first_name: "aerith", last_name: "gainsborough", display_name: "aerith")
+User.create!(email: "fuhajin@gmail.com", first_name: "juri", last_name: "han", display_name: "jurihan", password: "password")
+User.create(email: "littlefriend@gmail.com", first_name: "tony", last_name: "montana", display_name: "scarface", password: "password")
+User.create(email: "quietlife@gmail.com", first_name: "yoshikage", last_name: "kira", display_name: "killerqueen", password: "password")
+User.create(email: "baddad@gmail.com", first_name: "gendo", last_name: "ikari", display_name: "gendopose", password: "password")
+User.create(email: "beauty@gmail.com", first_name: "audrey", last_name: "hepburn", display_name: "romanholiday", password: "password")
+User.create(email: "pokerface@gmail.com", first_name: "stefani", last_name: "germanotta", display_name: "ladygaga", password: "password")
+User.create(email: "illustrious@gmail.com", first_name: "mari", last_name: "makinami", display_name: "zabeasto", password: "password")
+User.create(email: "bondvillian@gmail.com", first_name: "elon", last_name: "musk", display_name: "chieftwit", password: "password")
+User.create(email: "darkknight@gmail.com", first_name: "bruce", last_name: "wayne", display_name: "batman", password: "password")
+User.create(email: "bigguy4u@gmail.com", first_name: "tom", last_name: "hardy", display_name: "bane", password: "password")
+User.create(email: "cloudff7r@gmail.com", first_name: "cloud", last_name: "strife", display_name: "cloud", password: "password")
+User.create(email: "aerithff7r@gmail.com", first_name: "aerith", last_name: "gainsborough", display_name: "aerith", password: "password")
 
-client_id = "0dsiqx7nh6ljalvj1pev66afrkfu7w"
-access_token = "6mv7nk0h2ty0oqo2y3mx3mifbjye7q"
+10.times do
+  User.create!(email: Faker::Internet.unique.email, first_name: Faker::Name.unique.first_name, last_name: Faker::Name.unique.last_name, display_name: Faker::Name.unique.first_name + rand(10).to_s, password: "password")
+end
+
+client_id = ENV['CLIENT_ID']
+access_token = ENV["TOKEN"]
 
 http = Net::HTTP.new('api.igdb.com', 443)
 http.use_ssl = true
-request = Net::HTTP::Post.new(URI('https://api.igdb.com/v4/games'), {'Client-ID' => client_id, 'Authorization' => 'Bearer 6mv7nk0h2ty0oqo2y3mx3mifbjye7q'})
+request = Net::HTTP::Post.new(URI('https://api.igdb.com/v4/games'), {'Client-ID' => client_id, 'Authorization' => "Bearer #{access_token}"})
 request.body = 'fields name,cover.url,summary; where first_release_date < 946684799;'
 response = JSON.parse(http.request(request).body)
 
