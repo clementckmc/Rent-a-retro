b5fe2fd11316e08777381a748f001603fbace5c1
Alex V <alexandervasse@gmail.com>
2022-11-16 13:12:10 +0900

    index page showing but lacking filter and only showing gams with offers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_042810) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_082609) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -86,7 +86,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_042810) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "location"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
