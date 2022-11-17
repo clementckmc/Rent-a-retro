76b4385d5469b6c9d1fed92312a96ae15344459f
Patrick Stark <contact@patrick-stark.com>
2022-11-17 12:23:02 +0900

    added partial for rentals

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
+# @format
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
 
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -10,6 +15,7 @@ class User < ApplicationRecord
 
   has_many :offers, dependent: :destroy
   has_many :rentals, dependent: :destroy
+  has_many :rentals_incoming, through: :offers, source: :rentals
 
   has_one_attached :photo
 end
