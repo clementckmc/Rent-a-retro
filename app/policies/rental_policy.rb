5a57bfa62561dbd7ffe898b60b42d6db15843385
Patrick Stark <contact@patrick-stark.com>
2022-11-17 13:02:05 +0900

    added buttons to edit

--- app/policies/rental_policy.rb
+++ app/policies/rental_policy.rb
@@ -1,3 +1,5 @@
+# @format
+
 class RentalPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
@@ -5,10 +7,16 @@ class RentalPolicy < ApplicationPolicy
       scope.where(user: user)
     end
   end
+
   def show?
     true
   end
+
   def create?
+    true
+  end
+
+  def update?
     true
   end
 end
